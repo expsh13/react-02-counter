5f24e365358e9210290d533ddf8ce7bbe959d652
Satoki Hata <satokihata@gmail.com>
2024-02-06 23:08:35 +0900

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import { useState } from "react";
+
 export const App = () => {
-  return <div>App</div>;
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <p>カウンター</p>
+      <p>{count}</p>
+      <button onClick={() => setCount((count) => count + 1)}>+</button>
+      <button onClick={() => setCount((count) => count - 1)}>-</button>
+    </div>
+  );
 };
